c3df4a64b30ba5ad9210bd21c0a3dad79c800757
woowang789 <wangjh789@gmail.com>
2022-12-26 09:03:29 +0900

    add mapper test

---
+++ src/test/java/com/hyundai/mapper/MapperTests.java
@@ -0,0 +1,26 @@
+package com.hyundai.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class MapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private TestMapper mapper;
+	
+	@Test
+	public void test() {
+		int t = mapper.getInteger();
+		log.info(t);
+	}
+
+}
