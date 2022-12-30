fb7052b0c5f216c4a35773a28103347677cf2597
Gyu Seok <115447023+thrbtjr@users.noreply.github.com>
2022-12-30 14:46:11 +0900

    add product_detail page

--- src/main/java/com/hyundai/controller/ProductController.java
+++ src/main/java/com/hyundai/controller/ProductController.java
@@ -1,19 +1,42 @@
 package com.hyundai.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hyundai.mapper.ProductDetailMapper;
+import com.hyundai.vo.ProductDetailDTO;
 
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
 public class ProductController {
+	
+	@Autowired
+	private ProductDetailMapper mapper;
+	
+	
+	
 	@RequestMapping(value="/product-detail", method = RequestMethod.GET)
-	public String productDetail() {
+	public String productDetail(@RequestParam String pid, Model model) {
 		log.info("/product-detail");
+	model.addAttribute("product_detail",mapper.getProductDetail(pid));
 		
 		return "product/product_detail";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/product-detail2", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ProductDetailDTO productDetail2(@RequestParam String pid, Model model) {
+		log.info("/product-detail: " + pid);
+		
+		return mapper.getProductDetail(pid);
 	}
 
 }
---
+++ src/main/java/com/hyundai/mapper/ProductDetailMapper.java
@@ -0,0 +1,9 @@
+package com.hyundai.mapper;
+
+import com.hyundai.vo.ProductDetailDTO;
+
+public interface ProductDetailMapper {
+	
+	public ProductDetailDTO getProductDetail(String pid);
+
+}
